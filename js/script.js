4efe4c253a59d31adf1fd2681ae03b44f3a29801
nana <goldbarrr5@gmail.com>
2023-12-13 15:34:50 +0900

    비주얼 슬라이드

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.visual', {
+  pagination: {
+    el: '.swiper-dot',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pauseOnMouseEnter: true,
+});
