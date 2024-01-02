4a04b556f2c15bcdb504e669fa51ccbacea9fefe
nana <goldbarrr5@gmail.com>
2024-01-02 17:06:43 +0900

    event---

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.visual', {
+  // visual slide
+  pagination: {
+    el: '.swiper-dot',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pauseOnMouseEnter: true,
+});
+
+// event slide
+var eventSwiper = new Swiper('.event-image-wrap', {
+  effect: 'cards',
+  grabCursor: true,
+  rotate: true,
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+});
