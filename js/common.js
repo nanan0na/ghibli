dc1c4a9447b8f251ba537a091a5d00e9a543a809
nana <goldbarrr5@gmail.com>
2024-01-08 20:47:51 +0900

    햄버거 메뉴 한 화면에 들어오게 수정해야 함

--- js/common.js
+++ js/common.js
@@ -7,7 +7,14 @@ $(function () {
   // menu
   $('.btn-menu').on('click', function () {
     const menuOpen = $('<div class="menu-open"></div>');
-    $('.btn-menu').append(menuOpen);
+    if ($('.util').children('.menu-open').length === 1) {
+      $('.util').append(menuOpen);
+    } else if ($('.util').children('.menu-open').length === 0) {
+      $('.util .menu-open').remove();
+    }
+    $(
+      '.logo a, .gnb, .gnb li a, .menu-open, .toggle-lang, .btn-menu, .line, .toggle-lang-change, .headerSns'
+    ).toggleClass('active');
   });
 
   // cast
